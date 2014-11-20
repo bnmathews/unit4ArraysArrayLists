ee425be3ad9d2b83fa7806596cb854ee1f1f38e2
Ben Mathews <bnmathews@student.naperville203.org>
2014-11-20 12:21:55 -0600

    More Array Methods

--- Ch7Practice/ArrayMethods.java
+++ Ch7Practice/ArrayMethods.java
@@ -20,19 +20,12 @@ public class ArrayMethods
     
     public void replaceNeighbors()
     {
-        for (int i = 0; i < values.length; i++)
+        int prevVal = values[0];
+        for (int i = 1; i < values.length - 1; i++)
         {
-            if ( i != 0 && i != (values.length - 1) )
-            {
-                if ( (values[i - 1] > values[i]) && (values[i - 1] > values[i + 1]) )
-                {
-                    values[i] = values[i - 1];
-                }
-                else if ( (values[i + 1] > values[i]) && (values[i + 1] > values[i - 1]) )
-                {
-                    values[i] = values[i + 1];
-                }
-            }
+            int tempVal = values[i];
+            //
+            prevVal = tempVal;
         }
     }
     
@@ -40,11 +33,12 @@ public class ArrayMethods
     {
         if (values.length % 2 == 0)
         {
-            
+            values[(int)(values.length / 2)] = 0;
+            values[(int)((values.length / 2) - 1)] = 0;
         }
         else
         {
-            System.out.println("The middle value is: " + values.length/2 + ", or" + values[values.length / 2]); 
+            values[values.length / 2] = 0;
         }
     }
     
@@ -63,6 +57,45 @@ public class ArrayMethods
         int tempVal = values[0];
         values[0] = values[values.length - 1];
         values[values.length - 1] = tempVal;
+    }
+    
+    public void moveEvens()
+    {
+        int[] newArray = new int[values.length];
+        int arrayCount = 0;
+        String str = "";
+        for (int i = 0; i < values.length; i++)
+        {
+            if (values[i] % 2 == 0)
+            {
+                newArray[arrayCount] = values[i];
+                arrayCount++;
+            }
+        }
+        for (int i = 0; i < values.length; i++)
+        {
+            if (values[i] % 2 != 0)
+            {
+                newArray[arrayCount] = values[i];
+                arrayCount++;
+            }
+        }
+        values = newArray;
+    }
+    
+    public int secondLargest()
+    {
+        int large = 0;
+        int larger = 0;
+        for (int i = 0; i < values.length; i++)
+        {
+            for (int k = 0; k < values.length; k++)
+            {
+                
+            }
+        }
+        
+        return large;
     }
     
     public String toString()
